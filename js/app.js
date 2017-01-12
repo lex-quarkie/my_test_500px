ee76088fc82e674e34d2ffcd6047e752b837c16a
Lex Quarkie <lex.quarkie@gmail.com>
2017-01-12 18:54:21 +0300

    fetch done, photo added, need css, need Photoview

--- js/app.js
+++ js/app.js
@@ -1,108 +1,138 @@
-
-var examplePhoto =  {
-      "id": 191071963,
-      "user_id": 324706,
-      "name": "Sierra Awakens",
-      "description": "Definitely without question the most photographed place I have ever photographed.  That's the Alabama Hills, right here.  In fact, they may actually be more photographed than any landscape in history due to all the big Hollywood filmmakers which have come hear over the past century, to this rugged land of rocks and striking peaks.  \n\nI have lead more than fifty week-long photographic workshops and tours in this region over more than a decade now and because of this I preach creativity and exploration.  Perhaps no region I've been offers more diverse opportunities for shooting, and someone shooting with me could most easily avoid iconic or classic locations and get dozens of portfolio shots in a trip.  But not here.  Nope, this is taken right from main street in the Alabama's.  It's not another shot of Mobius Arch, thankfully, but an equally recognizable background looms here out of the clearing storm adorned with a small barrel cactus up front.   Lone Pine peak shown here is the most prominent peak in the area, thanks to its incredible light.  Never was that more true than on this morning either, so I just couldn't resist sharing this.  \n\nI have only processed one personal image in the last several months, so I have been taking a bit of a break here, focusing on family.  I am now getting back into the swing of things and am offering this shot as a bit of a 'warm-up' for what's to come...",
-      "camera": null,
-      "lens": null,
-      "focal_length": null,
-      "iso": null,
-      "shutter_speed": null,
-      "aperture": null,
-      "times_viewed": 37567,
-      "rating": 99.9,
-      "status": 1,
-      "created_at": "2017-01-02T15:00:39-05:00",
-      "category": 8,
-      "location": null,
-      "latitude": null,
-      "longitude": null,
-      "taken_at": null,
-      "hi_res_uploaded": 0,
-      "for_sale": false,
-      "width": 1092,
-      "height": 1600,
-      "votes_count": 2757,
-      "favorites_count": 0,
-      "comments_count": 107,
-      "nsfw": false,
-      "sales_count": 0,
-      "for_sale_date": null,
-      "highest_rating": 99.9,
-      "highest_rating_date": "2017-01-03T01:32:04-05:00",
-      "license_type": 0,
-      "converted": 27,
-      "collections_count": 159,
-      "crop_version": 2,
-      "privacy": false,
-      "profile": true,
-      "image_url": "https://drscdn.500px.org/photo/191071963/q%3D50_w%3D140_h%3D140/ef7c57d7613b94bf82f529715bce41f0?v=2",
-      "images": [
-        {
-          "size": 2,
-          "url": "https://drscdn.500px.org/photo/191071963/q%3D50_w%3D140_h%3D140/ef7c57d7613b94bf82f529715bce41f0?v=2",
-          "https_url": "https://drscdn.500px.org/photo/191071963/q%3D50_w%3D140_h%3D140/ef7c57d7613b94bf82f529715bce41f0?v=2",
-          "format": "jpeg"
-        }
-      ],
-      "url": "/photo/191071963/sierra-awakens-by-marc-adamus",
-      "positive_votes_count": 2757,
-      "converted_bits": 27,
-      "watermark": false,
-      "image_format": "jpeg",
-      "user": {
-        "id": 324706,
-        "username": "MAPhoto",
-        "firstname": "Marc ",
-        "lastname": "Adamus",
-        "city": "Anywhere",
-        "country": "Earth",
-        "usertype": 11,
-        "fullname": "Marc  Adamus",
-        "userpic_url": "https://pacdn.500px.org/324706/9fdb6516fab9a65a6bad39c282732c4f8ab7197c/1.jpg?7",
-        "userpic_https_url": "https://pacdn.500px.org/324706/9fdb6516fab9a65a6bad39c282732c4f8ab7197c/1.jpg?7",
-        "cover_url": "https://pacdn.500px.org/324706/9fdb6516fab9a65a6bad39c282732c4f8ab7197c/cover_original.jpg?13",
-        "upgrade_status": 0,
-        "store_on": false,
-        "affection": 602542,
-        "avatars": {
-          "default": {
-            "https": "https://pacdn.500px.org/324706/9fdb6516fab9a65a6bad39c282732c4f8ab7197c/1.jpg?7"
-          },
-          "large": {
-            "https": "https://pacdn.500px.org/324706/9fdb6516fab9a65a6bad39c282732c4f8ab7197c/2.jpg?7"
-          },
-          "small": {
-            "https": "https://pacdn.500px.org/324706/9fdb6516fab9a65a6bad39c282732c4f8ab7197c/3.jpg?7"
-          },
-          "tiny": {
-            "https": "https://pacdn.500px.org/324706/9fdb6516fab9a65a6bad39c282732c4f8ab7197c/4.jpg?7"
-          }
-        }
-      },
-      "licensing_requested": false,
-      "licensing_suggested": false,
-      "is_free_photo": false
+((React, fetch, Redux) => {
+    
+  // Redux actions
+  const actions = {
+    SELECT: 'SELECT_photo'
   };
-var link = 'https://api.500px.com/v1/photos?feature=popular&consumer_key=wB4ozJxTijCwNuggJvPGtBGCRqaZVcF6jsrzUadF';
-var App = React.createClass({
-	render: function() {
-		return (
-			<div className="app">
-			{examplePhoto.id}
-			<br />
-			<img src={examplePhoto.image_url} />
-			<img src={examplePhoto.url} />
+  
+  // Redux store
+  const reducer = (state, action) => {
+    switch (action.type) {
+        case actions.SELECT:
+          return {photo: action.photo};
+        default:
+          return {photo: null}
+    }
+  };
+  const photoStore = Redux.createStore(reducer);
 
+  // photository component
+  class Photo extends React.Component {
 
+    render() {
+      let photo = (<div>No photo selected</div>);
+      if (this.props.photo) {
+        photo = (
+          <div>
+            <h2>{this.props.photo.name}</h2>
+            <p>{this.props.photo.description}</p>
+            <a href="{this.props.photo.html_url}">See me on Github</a>
+          </div>
+        )
+      }
+      return (
+        <div className="photo">{photo}</div>  
+      )
+    }
 
-			</div>
-			);
-	}
-});
+  }
 
-ReactDOM.render(
-	<App />,
-	document.getElementById('root')
-	);
+  // photos component
+  class Photos extends React.Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        photos: [],
+        consumer_key: 'wB4ozJxTijCwNuggJvPGtBGCRqaZVcF6jsrzUadF',
+        feature: "popular"
+
+      };
+    }
+
+    componentDidMount() {
+      let self = this;
+      let url = `https://api.500px.com/v1/photos?feature=${this.state.feature}&consumer_key=${this.state.consumer_key}`
+      console.log(url)
+      fetch(url)
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+          self.setState({
+            photos: json.photos
+          });
+        })
+        .catch((e) => {
+          console.log('Error while retrieving photos', e);
+        });
+    }
+
+    render() {
+      let photos, self = this;
+      if (self.state.photos.length === 0) {
+        photos = (<div>loading...</div>)
+      }
+      else {
+        let photosList = self.state.photos.map((photo) => {
+          return (
+            <li onClick={() => self.selectphoto(photo).bind(self)}>
+              {photo.name}
+              <img src={photo.image_url} />
+            </li>
+            );
+        });
+        photos = (<ul>{photosList}</ul>)
+      }
+      return (
+        <div className="photos">{photos}</div>
+      );
+    }
+    
+    selectphoto(photo) {
+      this.props.store.dispatch({
+        type: actions.SELECT,
+        photo: photo
+      });
+    }
+
+  }
+
+  // App component
+  class Gallery extends React.Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        selectedPhoto: null
+      };
+    }
+
+    componentDidMount() {
+      this.unsubscribe = photoStore.subscribe(this.onphotoSelected.bind(this));
+    }
+
+    componentWillUnmount() {
+      this.unsubscribe();
+    }
+
+    onphotoSelected() {
+      this.setState({
+        selectedPhoto: photoStore.getState().photo
+      });
+    }
+
+    render() {
+      return (
+        <div>
+          <Photos store={photoStore}/>
+          <Photo photo={this.state.selectedPhoto} store={photoStore}/>
+        </div>
+      )
+    }
+
+  }
+
+  // Render application
+  React.render(<Gallery/>, document.getElementById('app'));
+  
+})(React, fetch, Redux);
